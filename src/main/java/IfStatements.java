a7ea0480244f43ba21adc2b6137be9c4c79baeac
BurnsN-Prog <ndburns222@gmail.com>
2024-12-06 17:54:37 +0000

    Completed IfStatements

--- src/main/java/IfStatements.java
+++ src/main/java/IfStatements.java
@@ -12,7 +12,10 @@ public class IfStatements {
      * @return - x if bool is true, otherwise return y.
      */
     public int exercise1(boolean bool, int x, int y) {
-        return 0;
+    if (bool == true){
+        return x;}
+    else{
+        return y;}
     }
 
     /**
@@ -22,7 +25,15 @@ public class IfStatements {
      * @return - "Positive", "Negative", or "Zero" depending on the input.
      */
     public String exercise2(int x) {
-        return null;
+    if (x > 0){
+        return "Positive";
+    }
+    if (x == 0){
+        return "Zero";
+    }
+    else{
+        return "Negative";
+    }
     }
 
     /**
@@ -39,7 +50,17 @@ public class IfStatements {
      * @param year - an integer representing a year.
      * @return - "Yes", "No", or "Not quite!" depending on the input. 
      */
-    public String exercise3(int year) {
-        return null;
+    public String exercise3(int year) {    
+    if ( year % 4 != 0){
+        return "No";}
+        if (year % 4 == 0 && year % 100== 0 && year % 400 != 0){
+            return "Not quite";}
+            if (year == 0 && year % 100 == 0 && year % 400 == 0){
+                return "Yes";}
+    else{
+        return "Yes";
     }
+        
+    }    
 }
+
